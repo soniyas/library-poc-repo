b57b5bb6c776135e439aef76713a18ab806755e6
soniyas <soniya.vjy@gmail.com>
2021-11-12 23:54:29 +0000

    added pom

---
+++ library-poc/src/main/java/com/demo/librarypoc/LibraryPocApplication.java
@@ -0,0 +1,13 @@
+package com.demo.librarypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPocApplication.class, args);
+	}
+
+}
---
+++ library-poc/src/test/java/com/demo/librarypoc/LibraryPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.librarypoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
