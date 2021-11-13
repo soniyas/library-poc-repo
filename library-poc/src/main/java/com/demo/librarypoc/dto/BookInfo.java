98a057294f446c8d7a2eef55d22657c8765279bd
soniyas <soniya.vjy@gmail.com>
2021-11-13 21:54:12 +0000

    import explicitly

--- library-poc/src/main/java/com/demo/librarypoc/dto/BookInfo.java
+++ library-poc/src/main/java/com/demo/librarypoc/dto/BookInfo.java
@@ -1,6 +1,11 @@
 package com.demo.librarypoc.dto;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
